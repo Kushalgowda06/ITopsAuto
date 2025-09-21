076f37c846c245819872a4fca82e95297b6fe19a
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-09-21 11:32:50 +0000

    Update resolution_assist_routes.py

--- Backend/resolution_management/resolution_assist_routes.py
+++ Backend/resolution_management/resolution_assist_routes.py
@@ -4,11 +4,8 @@ import json
 import traceback
 
 from fastapi import APIRouter
-from typing import List, Optional
-from pydantic import Field, BaseModel
+from pydantic import BaseModel
 from fastapi.responses import JSONResponse
-import os
-import json
 
 # Get project root (Backend/)
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -18,7 +15,6 @@ ROOT_PATH = os.path.abspath(os.path.join(SCRIPT_PATH, os.pardir))
 mim_conf_path = os.path.join(ROOT_PATH, "config", "mim_conf.json")
 with open(mim_conf_path, "r") as f:
     mim_conf = json.load(f)
-    mim_conf_file.close()
 
 from vault import Vault
 from llm_app.llm_utils import LLMUtils
@@ -38,41 +34,42 @@ class TechAssistPayload(BaseModel):
     context: dict
 
 
-@router.post(f"/api/{mim_conf['api_version']}/act_for_demo_on_ask", status_code = 200)
+@router.post(f"/api/{mim_conf['api_version']}/act_for_demo_on_ask", status_code=200)
 async def ask_llm_to_act_for_demo(payload: TechAssistPayload):
     query = payload.query
     context = payload.context
 
     last_activity = "user: " + query
 
-    # Call the task that acts on end machines
     response = {}
-
     try:
         act_instance = LLMUtils()
         (output, context) = act_instance.ask_llm_to_act_for_demo(query, context)
 
         last_activity += "\nsystem: " + output
 
-        summary_prompt = "Read the following conversation delimited by 3 back ticks - ```\n\n" + last_activity + "\n\n```\n\n"
-        summary_prompt += "Summarize the conversation to update as work notes."
+        summary_prompt = (
+            "Read the following conversation delimited by 3 back ticks - ```\n\n"
+            + last_activity
+            + "\n\n```\n\nSummarize the conversation to update as work notes."
+        )
 
         work_notes_or_comments = act_instance.ask_llm(summary_prompt)
 
-        response["output"] = {"data": {"response": output,
-                                       "context": context,
-                                       "work_notes_or_comments": work_notes_or_comments
-                                       },
-                              "message": "Response retrieved successfully."
-                              }
+        response["output"] = {
+            "data": {
+                "response": output,
+                "context": context,
+                "work_notes_or_comments": work_notes_or_comments,
+            },
+            "message": "Response retrieved successfully.",
+        }
 
         response["code"] = 200
-        return JSONResponse(status_code = 200, content = response)
+        return JSONResponse(status_code=200, content=response)
+
     except Exception as e:
         response["error"] = {"data": {}, "message": str(e)}
         response["code"] = 500
-
         traceback.print_exc()
-
-        return JSONResponse(status_code = 500, content = response)
-
+        return JSONResponse(status_code=500, content=response)
