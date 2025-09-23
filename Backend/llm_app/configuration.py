1d0ebea3e23416598a2e819ba181704472148827
Kushalgowda06 <kushalgowda.s@cognizant.com>
2025-09-23 05:56:18 +0000

    Update configuration.py

--- Backend/llm_app/configuration.py
+++ Backend/llm_app/configuration.py
@@ -10,14 +10,16 @@ config={
       "kb_postgre_password":"postgre_password",
       "kb_postgre_host":"host",
       "kb_postgre_port":"port",
-      "openai_key":"0911999a1afe4ccdaa361cb2c801ee11",
-      "openai_api_version":"2025-01-01-preview",
-      "openai_azure_enpoint":"https://aiinfusedterraformmodel.cognitiveservices.azure.com/",
-      "openai_model":"gpt-4o",
+      # "openai_key":"0911999a1afe4ccdaa361cb2c801ee11",
+      # "openai_api_version":"2025-01-01-preview",
+      # "openai_azure_enpoint":"https://aiinfusedterraformmodel.cognitiveservices.azure.com/",
+      # "openai_model":"gpt-4o",
       "openai_key":"5mOo4VEjJoEvVXKlaeBp62fFNw0rowW37dTsVNju6EgTSToisopPJQQJ99BHACHYHv6XJ3w3AAAAACOG7T2P",
       "openai_api_version":"2025-01-01-preview",
       "openai_azure_enpoint":"https://techbuddy-az-openai.cognitiveservices.azure.com/",
       "openai_model":"gpt-4o",
       "api_type":"azure"
+      }
+
       }
 
